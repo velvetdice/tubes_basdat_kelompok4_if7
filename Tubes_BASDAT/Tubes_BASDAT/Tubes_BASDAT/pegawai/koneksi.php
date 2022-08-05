597eb51ac36393b2eb1349d40450281333c10e69
velvetdice <110656576+velvetdice@users.noreply.github.com>
2022-08-05 16:23:44 +0700

    Update koneksi.php

--- Tubes_BASDAT/Tubes_BASDAT/Tubes_BASDAT/pegawai/koneksi.php
+++ Tubes_BASDAT/Tubes_BASDAT/Tubes_BASDAT/pegawai/koneksi.php
@@ -1,6 +1,6 @@
 <?php
 // (nama server, user, password, database)
-$mysqli = new mysqli("localhost", "root", "", "tubes_basdat");
+$mysqli = new mysqli("localhost", "root", "", "kelompok4_if7_kepegawaian");
 /* check connection */
 if (mysqli_connect_errno()) {
 	printf("Connect failed: %s\n", mysqli_connect_error());
